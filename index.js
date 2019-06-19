bf640133f45912d5029bb4d0f18c5a76b8361c16
Matej Kamenec <mates.kamenec@gmail.com>
2019-06-19 02:09:09 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,56 @@
+"use strict";
+exports.__esModule = true;
+function validateCustomFormat(value, formats, useACharAsUniversalLetter) {
+    if (useACharAsUniversalLetter === void 0) { useACharAsUniversalLetter = false; }
+    if (!value || !formats) {
+        return true;
+    }
+    if (!value.length || !formats.length) {
+        return true;
+    }
+    for (var _i = 0, formats_1 = formats; _i < formats_1.length; _i++) {
+        var i = formats_1[_i];
+        var valid = validateByFormat(i, value, useACharAsUniversalLetter);
+        if (valid) {
+            return true;
+        }
+    }
+    return false;
+}
+exports.validateCustomFormat = validateCustomFormat;
+function validateByFormat(format, value, useACharAsUniversalLetter) {
+    var chars = format.toUpperCase().split('');
+    var regex = [];
+    for (var _i = 0, chars_1 = chars; _i < chars_1.length; _i++) {
+        var c = chars_1[_i];
+        if (isLetter(c)) {
+            if (useACharAsUniversalLetter) {
+                if (c === 'A') {
+                    regex.push('([a-zA-Z])');
+                }
+            }
+            else {
+                regex.push('([' + c + '])');
+            }
+        }
+        else if (c === '*') {
+            regex.push('(.)');
+        }
+        else if (c === ' ') {
+            regex.push('(\\s)');
+        }
+        else {
+            if (c === '0') {
+                regex.push('([0-9])');
+            }
+            if (c === '1') {
+                regex.push('([1-9])');
+            }
+        }
+    }
+    var regExp = new RegExp('^' + regex.join('') + '$');
+    return regExp.test(value);
+}
+function isLetter(str) {
+    return str.length === 1 && str.match(/[A-Z]/i);
+}
---
+++ index.ts
@@ -0,0 +1,49 @@
+
+export function validateCustomFormat(value: string, formats: string[], useACharAsUniversalLetter: boolean = false) {
+    if (!value || !formats) {
+        return true;
+    }
+
+    if (!value.length || !formats.length) {
+        return true;
+    }
+
+    for (const i of formats) {
+        const valid: boolean = validateByFormat(i, value, useACharAsUniversalLetter);
+        if (valid) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function validateByFormat(format: string, value: string, useACharAsUniversalLetter: boolean): boolean {
+    const chars: string[] = format.toUpperCase().split('');
+
+    const regex = [];
+
+    for (const c of chars) {
+        if (isLetter(c)) {
+            if (useACharAsUniversalLetter) {
+                if (c === 'A') { regex.push('([a-zA-Z])'); }
+            } else {
+                regex.push('([' + c + '])');
+            }
+        } else if (c === '*') {
+            regex.push('(.)');
+        } else if (c === ' ') {
+            regex.push('(\\s)');
+        } else {
+            if (c === '0') { regex.push('([0-9])'); }
+            if (c === '1') { regex.push('([1-9])'); }
+        }
+    }
+
+    const regExp = new RegExp('^' + regex.join('') + '$');
+    return regExp.test(value);
+}
+
+function isLetter(str: string) {
+    return str.length === 1 && str.match(/[A-Z]/i);
+}
